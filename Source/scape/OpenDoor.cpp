d87d0502f0d3fc1105e93a90e6a2fc3579370a56
Gustavo Massaneiro <gustavomassa123@gmail.com>
2020-05-18 14:07:53 -0300

    More improvements and settings to Door component.

--- Source/scape/OpenDoor.cpp
+++ Source/scape/OpenDoor.cpp
@@ -1,7 +1,6 @@
 // Gustavo Massaneiro @ 2020
 
 #include "OpenDoor.h"
-#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -18,7 +17,29 @@ void UOpenDoor::BeginPlay()
 
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
-	TargetYaw += InitialYaw;
+	OpenAngle += InitialYaw;
+	//CloseAngle = InitialYaw;
+	LastOpened = 0.0f;
+	LastClosed = 0.0f;
+
+	if (!ensure(TriggerVolume))
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s: Trigger Volume is missing!"), *GetOwner()->GetName());
+	}
+
+	// Get the default actor based on the World top-down
+	auto DefaultPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!ensure(DefaultPawn))
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s: Failed to get Default Pawn!"), *GetOwner()->GetName());
+	}
+
+	// If TriggerActor is not defined, use Default Pawn
+	if (!TriggerActor)
+	{
+		TriggerActor = DefaultPawn;
+		UE_LOG(LogTemp, Warning, TEXT("%s: Using Default Pawn as Trigger Actor!"), *GetOwner()->GetName());
+	}
 }
 
 // Called every frame
@@ -26,10 +47,46 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Exponential Linear Interpolation for a smooth movement
-	CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, Speed);
+	// TODO: Use Event Driven instead of Pooling
+	if (ensure(TriggerVolume) && ensure(TriggerActor) && TriggerVolume->IsOverlappingActor(TriggerActor))
+	{
+		if ((GetWorld()->GetTimeSeconds() - LastClosed) >= OpenDelay)
+		{
+			OpenDoor(DeltaTime);
+			LastOpened = GetWorld()->GetTimeSeconds();
+		}
+	}
+	else
+	{
+		if ((GetWorld()->GetTimeSeconds() - LastOpened) >= CloseDelay)
+		{
+			CloseDoor(DeltaTime);
+			LastClosed = GetWorld()->GetTimeSeconds();
+		}
+	}
+}
 
-	if (CurrentYaw < (TargetYaw - 0.1f))
+void UOpenDoor::OpenDoor(float DeltaTime)
+{
+	// Exponential Linear Interpolation for a smooth movement
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, OpenAngle, DeltaTime, OpenSpeed);
+
+	if (CurrentYaw > 0.0f && CurrentYaw < (OpenAngle - 0.1f))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	}
+
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+}
+
+void UOpenDoor::CloseDoor(float DeltaTime)
+{
+	// Exponential Linear Interpolation for a smooth movement
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, InitialYaw, DeltaTime, CloseSpeed);
+
+	if (CurrentYaw < (InitialYaw - 0.1f))
 	{
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
 	}
--- Source/scape/OpenDoor.h
+++ Source/scape/OpenDoor.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/TriggerVolume.h"
+#include "Engine/World.h"
 #include "OpenDoor.generated.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
@@ -22,13 +26,36 @@ protected:
 public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
 
 private:
 	float InitialYaw;
 	float CurrentYaw;
+	float LastOpened;
+	float LastClosed;
 
 	UPROPERTY(EditAnywhere)
-	float TargetYaw = 90.0f;
+	float OpenAngle = 90.0f;
+
 	UPROPERTY(EditAnywhere)
-	float Speed = 1.5f;
+	float OpenSpeed = 1.5f;
+
+	UPROPERTY(EditAnywhere)
+	float OpenDelay = 0.0f;
+
+	//UPROPERTY(EditAnywhere)
+	//float CloseAngle = 0.0f;
+
+	UPROPERTY(EditAnywhere)
+	float CloseSpeed = 1.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CloseDelay = 0.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume *TriggerVolume;
+
+	UPROPERTY(EditAnywhere)
+	AActor *TriggerActor;
 };
